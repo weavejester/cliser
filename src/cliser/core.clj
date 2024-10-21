b48b4e41cb8b5202d4f9df10b949aea20806198d
James Reeves <jreeves@weavejester.com>
2024-10-21 12:20:10 +0100

    Add local-endpoints and with-endpoint macro

--- src/cliser/core.clj
+++ src/cliser/core.clj
@@ -1,6 +1,21 @@
 (ns cliser.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defprotocol Endpoint
+  (execute-on [endpoint env form]))
+
+(defrecord LocalEndpoint []
+  Endpoint
+  (execute-on [_ env form]
+    (eval `(let ~(reduce-kv conj [] env) ~@form))))
+
+(def local-endpoint ->LocalEndpoint)
+
+(defn- find-symbols [form]
+  (into #{} (filter symbol?) (tree-seq coll? seq form)))
+
+(defn- local-env [env form]
+  (let [symbols (filter (find-symbols form) (keys env))]
+    (reduce #(assoc %1 `(quote ~%2) %2) {} symbols)))
+
+(defmacro with-endpoint [endpoint & body]
+  `(execute-on ~endpoint ~(local-env &env body) (quote ~body)))
